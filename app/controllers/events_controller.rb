6f9827079ed7498841101f5597e40fa1c710b91e
angelo colardo <angelocolardo@gmail.com>
2019-07-02 18:22:00 +0200

    working on landing

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,30 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :photo)
+  end
+end
